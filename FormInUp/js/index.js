738dd254d94e02ddbfd091a18feb37400ca9deeb
Ilia1204 <lower013634@gmail.com>
2021-02-05 15:23:18 +0400

    version 1.6
    version 1.6

---
+++ FormInUp/js/index.js
@@ -0,0 +1,33 @@
+let showPassword = document.querySelectorAll('.showPassword');
+    showPassword.forEach(item =>
+        item.addEventListener('click', toggleType)
+    );
+function toggleType() {     
+    let input = this.closest('.dws-input2').querySelector('.password');
+    
+    if (input.type === 'password') {
+        input.type = 'text';
+    } 
+    else {
+        input.type = 'password';
+    }
+}
+
+$(document).ready(function(){
+    $("#setCookie").click(function () {
+    $.cookie("popup", "", { expires:0, path: '/' });
+    $("#bg_popup").hide();
+    });
+     
+    if ( $.cookie("popup") == null )
+    {
+    setInterval(function(){
+    $("#bg_popup").show();
+    }, 1000)
+    }
+    else { $("#bg_popup").hide();
+    }
+     
+});
+
+   
